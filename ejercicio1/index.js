6d9aa2ca62f4d678ddce253fb362388366a88976
albertferret <122560143+albertferret@users.noreply.github.com>
2023-01-26 18:53:47 +0100

    ejercicio1

---
+++ ejercicio1/index.js
@@ -0,0 +1,8 @@
+//base 16*23 cm
+//altura  13cm
+const volumen= 16*23*13;
+console.log('Volumen= ', volumen, 'cm3');
+const arealateral= 23*13;
+const areabase=16*23;
+const areatotal= arealateral+2*areabase;
+console.log('Area total= ', areatotal, 'cm2');
