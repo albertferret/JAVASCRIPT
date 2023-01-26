d65b48ae1b7e902bc5c8fa89aff2ef0c364e3d83
albertferret <122560143+albertferret@users.noreply.github.com>
2023-01-26 19:00:49 +0100

    Update index.js

--- ejercicio1/index.js
+++ ejercicio1/index.js
@@ -6,3 +6,12 @@ const arealateral= 23*13;
 const areabase=16*23;
 const areatotal= arealateral+2*areabase;
 console.log('Area total= ', areatotal, 'cm2');
+
+//Generalización
+const base1=23;
+const base2=16;
+const altura=13;
+const volumen2= base1*base2*altura
+console.log('Volumen2= ', volumen, 'cm3');
+const area2=base1*altura+(2*base1*base2);
+console.log('Area total2= ', area2, 'cm2');
