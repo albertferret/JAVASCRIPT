a3257de2aa0f90a1bc72acf69d23aff76147af87
albertferret <122560143+albertferret@users.noreply.github.com>
2023-01-26 19:33:02 +0100

    eje

---
+++ ejercico 2/index.js
@@ -0,0 +1,11 @@
+
+const meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre', 'Diciembre']
+const aniversario = {
+    dia: 11,
+    mes:meses[7]
+  
+  };
+  
+  console.log('Mi cumpleaños es el ' + aniversario.dia + ' de '+ meses[8]);
+  
+   
