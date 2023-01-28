1779f1c20a28420e247efaba74fd12617036ca65
albertferret <122560143+albertferret@users.noreply.github.com>
2023-01-28 19:35:42 +0100

    eje

--- ejercicio3/index.js
+++ ejercicio3/index.js
@@ -1,3 +1,9 @@
 const data = ['hola', 2, 5, 'adios'];
-const suma=data[1]+data[2];
-console log('Suma= ',suma);
+const numeroMayor=data[1]<data[2];
+console.log(numeroMayor);
+//const suma=data[1]+data[2];
+console.log("Suma =",data[1]+data[2]);
+console.log("Resta =",data[1]-data[2]);
+console.log("Producto =",data[1]*data[2]);
+console.log("Cociente =",data[1]/data[2]);
+console.log("Resto =",data[1]%data[2]);
