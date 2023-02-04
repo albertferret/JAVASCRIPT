87d2f9c3dcf73662785b605df74e7be2f9a3b5d7
albertferret <122560143+albertferret@users.noreply.github.com>
2023-02-04 19:01:08 +0100

    conte

--- ejercicio14/index.js
+++ ejercicio14/index.js
@@ -1,3 +1,4 @@
 const meses = ['Enero','Febrero','Marzo','Abril','Marzo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
-const meseslargos = meses.filter (meses => meses.length>7).map(meses => meses.toUpperCase());
+const meseslargos = meses.filter (meses => meses.length>7).map(meses => meses.toUpperCase())
 console.log(meseslargos);
+
--- ejercicio15/index.js
+++ ejercicio15/index.js
@@ -5,4 +5,11 @@ const users = [
     {username: 'abc123', age:20, premium:true},
     {username: 'sergio58', age:26, premium:true},
 ];
-const 
+const userpremium =users.filter (function(user){
+    return user.username=true;
+});
+console.log(userpremium);
+//const nopremium= users.filter (function(user2){
+//    return user2.premium=false;
+ //   });
+//console.log(nopremium);
