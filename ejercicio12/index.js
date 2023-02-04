599c32d660b9bb1674921e5ce62cca1c7882f0b1
albertferret <122560143+albertferret@users.noreply.github.com>
2023-02-04 09:42:28 +0100

    ejer

--- ejercicio12/index.js
+++ ejercicio12/index.js
@@ -1,4 +1,5 @@
-const topics = ['JavaScript', 'Variables', “funciones”, “condicionales”, “bucles”];
-topics.reverse()
-console.log(topics)
+const topics = ['JavaScript', 'Variables', 'funciones', 'condicionales', 'bucles'];
+const topics1=topics.reverse();
+topics2 = topics1.map(function(x){ return x.toUpperCase(); })
+console.log(topics2)
 
